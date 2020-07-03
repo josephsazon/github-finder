0ab409e06ac3ef133ca8e7d052f4ee00388e7cd7
Joseph Sazon <jsazon427@gmail.com>
2020-07-03 16:24:21 +0800

    create user item component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class UserItem extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div>User Item</div>;
+  }
+}
+
+export default UserItem;
