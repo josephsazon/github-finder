5c682adc3513c4ac7ad18d395985936d00ec0a1a
Joseph Sazon <jsazon427@gmail.com>
2020-07-04 13:47:35 +0800

    add github api and map state to users props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 // components
 import Navbar from "./components/layout/Navbar";
@@ -8,12 +9,22 @@ import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((response) => {
+      this.setState({ users: response.data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} />
         </div>
       </div>
     );
