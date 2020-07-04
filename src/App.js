62c6e418c61dcf787cf2115c15e4598fc4e759e5
Joseph Sazon <30966483+josephsazon@users.noreply.github.com>
2020-07-04 14:15:04 +0800

    feat: components/users - add github api and map to state (#7)
    * add github api and map state to users props
    
    * add loading state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 // components
 import Navbar from "./components/layout/Navbar";
@@ -8,12 +9,34 @@ import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    usersLoading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ usersLoading: true });
+    axios
+      .get("https://api.github.com/users")
+      .then((response) => {
+        this.setState({ users: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        this.setState({ usersLoading: false });
+      });
+  }
+
   render() {
+    const { users, usersLoading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users users={users} usersLoading={usersLoading} />
         </div>
       </div>
     );
