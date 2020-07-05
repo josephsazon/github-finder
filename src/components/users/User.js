0c000c04ee0cc898821f92e9f004fd506b3f395c
Joseph Sazon <jsazon427@gmail.com>
2020-07-05 18:51:08 +0800

    add proptypes and default props

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 // components
 import Spinner from "../layout/Spinner";
 
 class User extends Component {
+  static propTypes = {
+    getUser: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    userLoading: PropTypes.bool.isRequired,
+  };
+
+  static defaultProps = {
+    user: {},
+    userLoading: false,
+  };
+
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
 
   render() {
-    const { user, userLoading } = this.props;
+    const { name } = this.props.user;
+    const { userLoading } = this.props;
 
-    return userLoading ? <Spinner /> : <div>{user.name}</div>;
+    return userLoading ? <Spinner /> : <div>{name}</div>;
   }
 }
 
