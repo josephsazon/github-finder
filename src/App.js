39a6eb8d6198a2a1aa3e66af52973e792dd9fd43
Joseph Sazon <30966483+josephsazon@users.noreply.github.com>
2020-07-06 20:28:57 +0800

    feat: pages/home - refactor home page (#31)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // state
@@ -8,10 +8,9 @@ import GithubState from "./context/github/GithubState";
 // components
 import About from "./components/pages/About";
 import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/users/Search";
 import User from "./components/users/User";
-import Users from "./components/users/Users";
 
 // styles
 import "./App.css";
@@ -24,18 +23,9 @@ const App = () => {
           <div className="App">
             <Navbar />
             <div className="container">
+              <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Alert />
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                ></Route>
+                <Route exact path="/" component={Home}></Route>
                 <Route exact path="/about" component={About}></Route>
                 <Route exact path="/user/:login" component={User}></Route>
               </Switch>
